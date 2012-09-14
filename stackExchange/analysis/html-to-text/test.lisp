5edb2558b554273345c6fdf5b1d5d3a50410868a
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-09-14 16:38:01 -0500

    Converted to streaming method

--- stackExchange/analysis/html-to-text/test.lisp
+++ stackExchange/analysis/html-to-text/test.lisp
@@ -1,9 +1,5 @@
 (defparameter *_DIR_* (directory-namestring *load-truename*))
-
 (defparameter *csv-name* "in.csv")
-(defparameter *txt* nil)
-(defparameter *csv* nil)
-(defparameter *processed-csv* nil)
 
 (cl-interpol:enable-interpol-syntax)
 
@@ -17,27 +13,25 @@
 (defun load-csv (path)
   (file-string path))
 
-(defun html->text (csv)
-  (with-cwd *_DIR_*
-    (loop for (posts_id body) in csv
-          for count from 1
-          do (let ((tmp-file "/tmp/tmp.txt"))
-               (with-open-file (strm tmp-file :direction :output :if-exists :supersede)
-                 (format strm "~a" body))
-               (format  t "converting line ~a~%" count)
-               (setf body [python html-to-text.py ?tmp-file ]))
-          collect (list posts_id body))))
+(let ((cnt 0))
+  (defun html->text (csv)
+    (incf cnt)
+    (when (and (not (= cnt 1))
+               (<= cnt 101))
+      (with-cwd *_DIR_*
+        (destructuring-bind (posts_id body) csv
+          (let ((tmp-file "/tmp/tmp.txt"))
+            (with-open-file (strm tmp-file :direction :output :if-exists :supersede)
+              (format strm "~a" body))
+            (format  t "converting line ~a with id ~a~%" cnt posts_id)
+            (setf body [python html-to-text.py ?tmp-file ]))
+          (list posts_id body))))))
 
-(setf *txt*
-      (load-csv 
-        (format nil "~a~a" *_DIR_* *csv-name*)))
+(defun write-csv (csv)
+  (with-open-file (strm "out.csv" :direction :output :if-exists :append :if-does-not-exist :create)
+    (awhen (html->text csv)
+      (cl-csv:write-csv-row it :stream strm))))
 
-(setf *csv* (csv->lst *txt*))
-(setf *csv* (subseq *csv* 0 100))
-
-(setf *processed-csv* (html->text *csv*))
-
-(with-cwd *_DIR_*
-  (with-open-file (strm "out.csv" :direction :output :if-exists :supersede)
-    (cl-csv:write-csv *processed-csv* :stream strm)))
+(with-open-file (strm (format nil "~a~a" *_DIR_* *csv-name*) :direction :input)
+  (cl-csv:read-csv strm :row-fn #'write-csv))
 
