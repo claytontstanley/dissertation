4fcf2a1f6c523adf6d888756d4cb18886a578df2
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-09-26 15:40:27 -0500

    initial dump of title chunks

---
+++ stackExchange/analysis/model/model.R
@@ -0,0 +1,9 @@
+# Setup variables and helper functions
+wideScreen(150)
+frameFiles = lapply(sys.frames(), function(x) x$ofile)
+frameFiles = Filter(Negate(is.null), frameFiles)
+PATH = dirname(frameFiles[[length(frameFiles)]])
+
+library(Matrix)
+
+
