fb90ff8fc70a43ba1cdc433dc7cacb0a80966552
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-09-28 12:52:31 -0500

    Optimized create-chunks

--- stackExchange/analysis/html-to-text/create-chunks.sql
+++ stackExchange/analysis/html-to-text/create-chunks.sql
@@ -4,10 +4,14 @@ DROP TABLE IF EXISTS sotero.`chunks` ;
 CREATE  TABLE IF NOT EXISTS sotero.`chunks` (
   `ChunkId` INT(11) NOT NULL ,
   `Id` INT(11) NOT NULL ,
-  `Chunk` LONGTEXT CHARACTER SET 'utf8' COLLATE utf8_general_ci NOT NULL ,
-  `ChunkType` LONGTEXT CHARACTER SET 'utf8' COLLATE utf8_general_ci NOT NULL ,
-   PRIMARY KEY (`ChunkId`) 
+  `Chunk` VARCHAR(250) CHARACTER SET 'utf8' NOT NULL ,
+  `ChunkType` VARCHAR(250) CHARACTER SET 'utf8' COLLATE utf8_general_ci NOT NULL ,
+   INDEX `ChunkIndex` (`Chunk` ASC),
+   INDEX `IDIndex` (`ID` ASC),
+   INDEX `ChunkTypeIndex` (`ChunkType` ASC),
+   PRIMARY KEY (`ChunkId`)
   )
 ENGINE = MyISAM
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
+
