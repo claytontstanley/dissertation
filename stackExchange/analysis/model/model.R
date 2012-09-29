a406d3b01afb00addc995d6f0fb3a004738306a8
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-09-29 18:24:40 -0500

    sjis are calculated

--- stackExchange/analysis/model/model.R
+++ stackExchange/analysis/model/model.R
@@ -7,12 +7,27 @@ PATH = dirname(frameFiles[[length(frameFiles)]])
 library(Matrix)
 library(stringr)
 
+sdiv <- function(X, Y, names=dimnames(X)) {
+  sX <- summary(X)
+  sY <- summary(Y)
+  sRes <- merge(sX, sY, by=c("i", "j"))
+  sparseMatrix(i=sRes[,1], j=sRes[,2], x=sRes[,3]/sRes[,4],
+               dimnames=names)
+}
 
 testCSV = read.csv(str_c(PATH, "/", "title-chunks.csv"), header=T, sep=",")
+testCSV$LeftChunk = as.character(testCSV$LeftChunk)
+testCSV$RightChunk = as.character(testCSV$RightChunk)
 
-print(testCSV)
+N = sparseMatrix(i=testCSV[[2]], j=testCSV[[4]], x=testCSV[[5]])
 
-N = sparseMatrix(i=testCSV[[2]], j=testCSV[[4]])
+NRowSums = rowSums(N, sparseResult = TRUE)
+NColSums = colSums(N, sparseResult = TRUE)
+NSum = sum(N)
+NProdSums = with(summary(N), sparseMatrix(i = i, j = j, x = rowSums(N)[i] * colSums(N)[j]))
+
+sji = NSum * sdiv(N, NProdSums)
+
 
 #print(testCSV)
 
