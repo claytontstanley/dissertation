ae2b446471003ad662e13aaa4995ef039241fa8b
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-10-05 16:26:21 -0500

    foo

--- stackExchange/analysis/model/model.R
+++ stackExchange/analysis/model/model.R
@@ -15,19 +15,28 @@ sdiv <- function(X, Y, names=dimnames(X)) {
                dimnames=names)
 }
 
-testCSV = read.csv(str_c(PATH, "/", "title-chunks.csv"), header=T, sep=",")
-testCSV$LeftChunk = as.character(testCSV$LeftChunk)
-testCSV$RightChunk = as.character(testCSV$RightChunk)
+hashOf = function(chunk, db) {
+	db[chunk,]$chunkHash
+}
 
-N = sparseMatrix(i=testCSV[[2]], j=testCSV[[4]], x=testCSV[[5]])
+colClasses=c("character", "integer", "character", "integer", "integer")
+testCSV = read.csv(str_c(PATH, "/", "title-chunks.csv"), header=T, sep=",",colClasses=colClasses)
 
-NRowSums = rowSums(N, sparseResult = TRUE)
-NColSums = colSums(N, sparseResult = TRUE)
+chunk = with(testCSV, rbind(data.frame(chunk=LeftChunk), data.frame(chunk=RightChunk)))
+chunkHash = with(testCSV, rbind(data.frame(chunkHash=LeftChunkHash), data.frame(chunkHash=RightChunkHash)))
+chunkHashFrame = data.frame(cbind(chunk, chunkHash))
+db = unique(chunkHashFrame)
+
+N = with(testCSV, sparseMatrix(i=LeftChunkHash, j=RightChunkHash, x=ChunkCount))
+
+NRowSums = rowSums(N, sparseResult=TRUE)
+NColSums = colSums(N, sparseResult=TRUE)
 NSum = sum(N)
-NProdSums = with(summary(N), sparseMatrix(i = i, j = j, x = rowSums(N)[i] * colSums(N)[j]))
+NProdSums = with(summary(N), sparseMatrix(i=i, j=j, x=rowSums(N)[i] * colSums(N)[j]))
 
 sji = NSum * sdiv(N, NProdSums)
 
+write.csv(summary(sji), file=str_c(PATH, "/", "sji.csv"))
+write.csv(summary(NProdSums), file=str_c(PATH, "/", "NProdSums.csv"))
 
-#print(testCSV)
 
