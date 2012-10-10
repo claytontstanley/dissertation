f8df6be3b4e2cf2c42adecdb0cd6248f1b701a7d
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-10-10 11:16:18 -0500

    foo

--- stackExchange/analysis/model/driver.R
+++ stackExchange/analysis/model/driver.R
@@ -1,5 +1,3 @@
-# Setup variables and helper functions
-wideScreen(150)
 frameFiles = lapply(sys.frames(), function(x) x$ofile)
 frameFiles = Filter(Negate(is.null), frameFiles)
 PATH = dirname(frameFiles[[length(frameFiles)]])
