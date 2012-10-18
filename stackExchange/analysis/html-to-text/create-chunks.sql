61e712bb583532aa16f85745d8562ae000c9ded4
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-10-18 13:24:11 -0500

    foo
    Former-commit-id: 43c964c1514425c20bfca239aa0098b218bb5b3d [formerly 9f1cdec22ab17f0b4171654cab9e578fa09d9bee]
    Former-commit-id: f64d6c62991d90e137af15a79ac2eaebc1d562b5

--- stackExchange/analysis/html-to-text/create-chunks.sql
+++ stackExchange/analysis/html-to-text/create-chunks.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS sotero.`chunks` (
   `ChunkId` INT(11) NOT NULL ,
   `ChunkHash` INT(11) NOT NULL ,
   `Id` INT(11) NOT NULL ,
-  `Chunk` VARCHAR(250) BINARY CHARACTER SET 'utf8' COLLATE utf8_general_ci NOT NULL ,
-  `ChunkType` VARCHAR(250) CHARACTER SET 'utf8' COLLATE utf8_general_ci NOT NULL ,
+  `Chunk` VARCHAR(255) BINARY CHARACTER SET 'utf8' COLLATE utf8_general_ci NOT NULL ,
+  `ChunkType` VARCHAR(255) CHARACTER SET 'utf8' COLLATE utf8_general_ci NOT NULL ,
    INDEX `ChunkHashIndex` (`ChunkHash` ASC),
-   INDEX `ChunkIndex` (`Chunk` ASC),
    INDEX `IDIndex` (`ID` ASC),
+   INDEX `ChunkIndex` (`Chunk` ASC),
    INDEX `ChunkTypeIndex` (`ChunkType` ASC),
    PRIMARY KEY (`ChunkId`)
   )
@@ -20,7 +20,7 @@ COLLATE utf8_general_ci;
 drop table if exists sotero.subsets;
 create table if not exists sotero.subsets (
 	Id int(11) not null,
-	Subset varchar(250) binary character set 'utf8' collate utf8_general_ci not null,
+	Subset varchar(255) binary character set 'utf8' collate utf8_general_ci not null,
 	primary key (Id, Subset)
  )
 ENGINE = MyISAM
