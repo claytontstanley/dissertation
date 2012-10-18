79d43b479419ff8ed7f919675a78c47036694076
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-10-18 14:12:42 -0500

    moved
    Former-commit-id: 3fdc7a096eed00969d9680793000204101abf197 [formerly 54ae344f8535c0d79259dab11028833fb5c4cbc8]
    Former-commit-id: 31f72ac6351f33499f469b7e29a445f54dedac05

--- stackExchange/analysis/html-to-text/update-chunks.sql
+++ stackExchange/analysis/html-to-text/update-chunks.sql
@@ -1,3 +1,82 @@
+DROP VIEW IF EXISTS sotero.chunkhashes;
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `chunkhashes` AS
+    select 
+        `chunks`.`Chunk` AS `Chunk`,
+        min(`chunks`.`ChunkId`) AS `ChunkHash`
+    from
+        `chunks`
+    group by `chunks`.`Chunk`
+    order by `chunks`.`ChunkId`;
+
+DELIMITER $$
+
+drop procedure if exists sotero.createChunkSubset;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `createChunkSubset`(theSubset varchar(255))
+BEGIN
+drop view if exists sotero.chunkSubset;
+drop view if exists sotero.idSubset;
+set @p1 := theSubset;
+create view sotero.idSubset as (select id from sotero.subsets where subset = p1());
+create view sotero.chunkSubset as
+	select c.* from sotero.chunks as c 
+	join sotero.idSubset as s 
+	on s.id = c.id;
+END;
+
+DROP PROCEDURE IF EXISTS sotero.title_chunks;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `title_chunks`(theSubset varchar(255))
+BEGIN
+call sotero.createChunkSubset(theSubset);
+select 
+	t.Chunk as LeftChunk,
+	t.ChunkHash as LeftChunkHash,
+	q.Chunk as RightChunk,
+	q.ChunkHash as RightChunkHash,
+	count(t.ChunkHash) as ChunkCount
+from
+	(select * from 
+		sotero.chunkSubset
+		where chunkType = "Title") as t
+	join
+	(select * from
+		sotero.chunkSubset
+		where chunkType = "Tag") as q
+	on
+		t.ID = q.ID
+group by LeftChunkHash, RightChunkHash
+order by t.ChunkID, q.ChunkID;
+drop view sotero.chunksubset;
+drop view sotero.idsubset;
+END;
+
+drop procedure if exists sotero.tag_priors;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `tag_priors`(theSubset varchar(255))
+BEGIN
+call sotero.createChunkSubset(theSubset);
+select 
+        `chunks`.`Chunk` AS `Chunk`,
+        `chunks`.`ChunkHash` AS `ChunkHash`,
+        count(`chunks`.`ChunkHash`) AS `ChunkCount`
+    from
+        `chunksubset` as chunks
+    where
+        (`chunks`.`ChunkType` = 'Tag')
+    group by `chunks`.`ChunkHash`
+    order by count(`chunks`.`ChunkHash`) desc , `chunks`.`ChunkHash`;
+drop view sotero.chunksubset;
+drop view sotero.idsubset;
+END;
+
+drop function if exists p1;
+CREATE DEFINER=`root`@`localhost` FUNCTION `p1`() RETURNS varchar(255) CHARSET utf8
+    NO SQL
+    DETERMINISTIC
+return @p1;
+
 update sotero.chunks as t
 	join sotero.chunkhashes as s 
 	on s.chunk = t.chunk
