cb675319721188e67ca2fb9866b4ba96334c781d
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-01-08 17:46:25 -0600

    foo
    Former-commit-id: c962f932d17d2a43318a0d0fd6218efe9698dccc [formerly 59d49643830bf2741a7e0beb8aa4d74f74f841b5]
    Former-commit-id: 5596413eb40633fae0339a24a21a0a7d3456b2e4

--- stackExchange/analysis/model/analyzer-spring.R
+++ stackExchange/analysis/model/analyzer-spring.R
@@ -7,3 +7,39 @@ PATH = dirname(frameFiles[[length(frameFiles)]])
 source(str_c(PATH, "/analyzer.R"))
 
 runSample = runLogReg("LogReg.csv", "runSample")
+
+
+visPost(tagFiles[2], runSample$coeffs)
+visPost(tagFiles[40], runSample$coeffs)
+visPost(tagFiles[120], runSample$coeffs)
+
+asFig("tagActDis")
+hist(as.vector(B[priorsIndeces]), main="Distribution of tag activations", xlab="Activation")
+devOff()
+
+asFig("tagActSorted")
+main="Activations of individual tags when sorted by activation"
+xlab="Sorted tag ID"
+ylab="Activation"
+plot(1:length(priorsIndeces), sort(as.vector(B[priorsIndeces]), decreasing=T), main=main, xlab=xlab, ylab=ylab)
+devOff()
+
+figName = "sjiActScatter"
+png(str_c(PATH, "/Pictures/", figName, ".png"), width=480*10, height=480*10, res=72*10)
+with(summary(sji), plot(i,j, main="Scatter of sji sparse matrix", xlab="i index", ylab="j index", cex=.2))
+devOff()
+
+asFig("attentionalN")
+main="Attentional weights as a function of #observations for each cue"
+xlab="log #observations for cue"
+ylab="Attentional weight"
+plot(log(as.vector(NRowSums[contextWeightsIndeces])), contextWeights[contextWeightsIndeces], main=main, xlab=xlab, ylab=ylab)
+textVect = c("php", "lisp", "the", "?", "xml", "foo", "very", "much", "well", "binding", "network", "a", "baz")
+textHash = getChunkHashes(textVect, db)
+textxy(log(as.vector(NRowSums[textHash])), contextWeights[textHash], textVect, cx=1, dcol = "orange")
+devOff()
+
+asFig("attentionalDis")
+hist(rep(as.vector(contextWeights[contextWeightsIndeces]), as.vector(NRowSums[contextWeightsIndeces])), main="Distribution of attentional weights", xlab="Weight")
+devOff()
+
