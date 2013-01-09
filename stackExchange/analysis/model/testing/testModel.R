e3f0f84f24477897699c5c65a4ad709a7cd8a4c6
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-01-09 17:28:43 -0600

    added oneContext
    Former-commit-id: 3f188379d5748580c336115bc09b9d316b4b39af [formerly a4583b4e53e6248f301f2d7b18788545178355ab]
    Former-commit-id: f4460090410f796e96ff7feb3ab3272d5ac9bf12

--- stackExchange/analysis/model/testing/testModel.R
+++ stackExchange/analysis/model/testing/testModel.R
@@ -18,5 +18,6 @@ writeStruct = function(struct, name=as.character(substitute(struct))) {
 res = list()
 res$standard = act(getChunkHashes(c("i", "want", "to", "learn", "php", "codez"), db), B, sji)
 res$noContext = act(getChunkHashes(c(), db), B, sji)
+res$oneContext = act(getChunkHashes(c("the"), db), B, sji)
 
 writeStruct(res)
