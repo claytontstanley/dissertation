d09ca0ba9d77e19982eec95f552b9224fd196557
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-01-09 17:19:11 -0600

    added regression tests for model
    Former-commit-id: b4733ffa9d6d082bd9df67b1585e9a2392c7f6d9 [formerly eb4380051f2de3d7d54c6c114105920e4e463986]
    Former-commit-id: 65d62c4503d1c1ae855b18451f1e04393ef39b64

---
+++ stackExchange/analysis/model/testing/testModel.R
@@ -0,0 +1,21 @@
+# Figure out the path to this file
+frameFiles = lapply(sys.frames(), function(x) x$ofile)
+frameFiles = Filter(Negate(is.null), frameFiles)
+PATH = dirname(frameFiles[[length(frameFiles)]])
+
+writeStruct = function(struct, name=as.character(substitute(struct))) {
+	if( mode(struct) == "list") {
+		for( element in names(struct)) {
+			writeStruct(struct[[element]], name=str_c(name, '-', element))
+		}
+	} else 	{
+		csvName = str_c(name, ".csv")
+		print(str_c("writing to csv: ", csvName))
+		write.csv(data.frame(vect=as.vector(struct[priorsIndeces])), file=str_c(PATH, "/", csvName))
+	}
+}
+
+res = list()
+res$standard = act(getChunkHashes(c("i", "want", "to", "learn", "php", "codez"), db), B, sji)
+
+writeStruct(res)
