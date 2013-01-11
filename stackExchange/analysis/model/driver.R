0391d5298fe3349f92baa860574081f2005d40da
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-01-11 13:41:06 -0600

    foo
    Former-commit-id: cca7f44544ff3d98f55fa906faa2aee05d88e2cb [formerly 30296bcf0f3e5b10f05763033a41b9a5d09d92e7]
    Former-commit-id: bf9b7e1386e7dafef362567835f0b5fca9754d86

--- stackExchange/analysis/model/driver.R
+++ stackExchange/analysis/model/driver.R
@@ -20,57 +20,19 @@ library(data.table)
 library(plyr)
 
 # A few helper functions
-rateVals = function(subsetIndeces, act, observed) {
-	vals = as.vector(act$act[subsetIndeces])
-	res = sort(vals, decreasing=T, index.return=T)
-	sortedChunkHashes = subsetIndeces[res$ix]
-	cutoff = 10
-	sortedChunkHashes = sortedChunkHashes[1:cutoff]
-	targetP = rep(0,cutoff)
-	targetP[match(observed, sortedChunkHashes)] = 1
-	sortedChunks = getChunks(sortedChunkHashes, db)
-	#myPrint(sortedChunkHashes)
-	#myPrint(observed)
-	return(data.frame(tag=sortedChunks, act=res$x[1:cutoff], targetP=targetP, rank=1:cutoff))
-}
-
-rateVals2 = function(subsetIndeces, act, observed, tagFile) {
-	vals = as.vector(act$sji[subsetIndeces] * 1.3 + B[subsetIndeces])
-	res = sort(vals, decreasing=T, index.return=T)
-	sortedChunkHashes = subsetIndeces[res$ix]
-	cutoff = min(length(res$ix), 1000)
-	revCutoff = length(res$ix) - cutoff + 1
-	#sortedChunkHashes = union(sortedChunkHashes[1:cutoff], sortedChunkHashes[revCutoff:length(sortedChunkHashes)])
-	sortedChunkHashes = sortedChunkHashes[1:cutoff]
-	sortedChunkHashes = union(sortedChunkHashes, observed)
-	targetP = rep(0, length(sortedChunkHashes))
-	target2P = targetP
-	observedVals = sort(vals[match(observed, subsetIndeces)], decreasing=T, index.return=T)
-	myPrint(observedVals)
-	myPrint(observed)
-	topValHash = observed[observedVals$ix[1]]
-	myPrint(topValHash)
-	targetP[match(observed, sortedChunkHashes)] = 1
-	target2P[match(topValHash, sortedChunkHashes)] = 1
-	sortedChunks = getChunks(sortedChunkHashes, db)
-	priors = as.vector(B[sortedChunkHashes])
-	sjis = as.vector(act$sji[sortedChunkHashes])
-	return(data.frame(tag=sortedChunks, sji=sjis, prior=priors, targetP=targetP, target2P=target2P, act=priors+sjis, tagFile=tagFile))
-}
-
-rateVals3 = function(subsetIndeces, act, observed, tagFile) {
+rateVals3 = function(priorsIndeces, act, observed, tagFile) {
 	cutoff = 200
-	vals = as.vector(B[subsetIndeces] + act$sjiTitle[subsetIndeces]*1.02 + act$sjiBody[subsetIndeces])*1.88
+	vals = as.vector(B[priorsIndeces] + act$sjiTitle[priorsIndeces]*1.02 + act$sjiBody[priorsIndeces])*1.88
 	res = sort(vals, decreasing=T, index.return=T)
-	sortedChunkHashes = subsetIndeces[res$ix]
+	sortedChunkHashes = priorsIndeces[res$ix]
 	#sortedChunkHashes = sortedChunkHashes[1:cutoff]
-	#sampleIndeces = union(union(observed, sortedChunkHashes), sample(subsetIndeces, 100, replace=F))
-	#sampleIndeces = union(sortedChunkHashes[1:cutoff], union(observed, sample(subsetIndeces, 500, replace=F, prob=vals)))
+	#sampleIndeces = union(union(observed, sortedChunkHashes), sample(priorsIndeces, 100, replace=F))
+	#sampleIndeces = union(sortedChunkHashes[1:cutoff], union(observed, sample(priorsIndeces, 500, replace=F, prob=vals)))
 	probs = 1/(1+exp(-vals/.5))
-	sampleIndeces = union(sortedChunkHashes[1:cutoff], union(observed, sample(subsetIndeces, 800, replace=F, prob=probs)))
+	sampleIndeces = union(sortedChunkHashes[1:cutoff], union(observed, sample(priorsIndeces, 800, replace=F, prob=probs)))
 	targetP = rep(0, length(sampleIndeces))
 	targetP[match(observed, sampleIndeces)] = 1
-	tags = getChunks(sampleIndeces, db)
+	tags = getChunks(sampleIndeces, dbPriors)
 	priors = as.vector(B[sampleIndeces])
 	sjiTitle = as.vector(act$sjiTitle[sampleIndeces])
 	sjiBody = as.vector(act$sjiBody[sampleIndeces])
@@ -95,13 +57,13 @@ ratePost = function(tagFile) {
 		observed = lst$observed
 		titleContext = lst$titleContext
 		bodyContext = lst$bodyContext
-		myPrint(str_c("working tag file ", tagFile))
+		print(str_c("working tag file ", tagFile))
 		tAct = act(getChunkHashes(titleContext, dbContext), B, sji)
 		bAct = act(getChunkHashes(bodyContext, dbContext), B, sji)
 		cAct = list()
 		cAct$sjiTitle = tAct$sji
 		cAct$sjiBody = bAct$sji
-		ret = rateVals3(priorsIndeces, cAct, getChunkHashes(observed, db), tagFile)
+		ret = rateVals3(priorsIndeces, cAct, getChunkHashes(observed, dbPriors), tagFile)
 		ret$tagFile=tagFile
 		res = rbind(res, ret)
 	}
@@ -137,8 +99,8 @@ W = 1
 tagFiles = getTagFiles(tagDir)
 
 # Run the model for each title/tag pair, and analyse results
-printP = 1
-#res = rbind.fill(parallel::mclapply(tagFiles, ratePost, mc.cores=2, mc.preschedule=T))
+printP = 0
+#res = rbind.fill(parallel::mclapply(tagFiles, ratePost, mc.cores=3, mc.preschedule=T))
 res = rbind.fill(lapply(tagFiles, ratePost))
 
 write.csv(res, file=str_c(PATH, "/LogReg.csv"))
@@ -147,4 +109,4 @@ write.csv(res, file=str_c(PATH, "/LogReg.csv"))
 # Save current objects so that they can be referenced from LaTeX document
 #save.image(file = str_c(PATH, "/", ".RData"))
 
-myPrint("done")
+print("done")
