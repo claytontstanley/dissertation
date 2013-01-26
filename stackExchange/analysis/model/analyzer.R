714c66de7f57d2323ebe357c53bcd5c581b24af5
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-01-25 19:14:46 -0600

    foo
    Former-commit-id: d3d0f4b6620753b4172cb2675b81d19d9b574719 [formerly 1e9d3a32fc52713592e2ae6cf4017fa2ad5040be]
    Former-commit-id: c80f4670c1ccc0126c82c9dcf9790ea4e977235c

--- stackExchange/analysis/model/analyzer.R
+++ stackExchange/analysis/model/analyzer.R
@@ -22,13 +22,10 @@ devOff = function() {
 	dev.off()
 }
 
-logReg = function(tag, res) {
+logReg = function(tag, res, model) {
 	myPrint(str_c("working ", length(tag), " tags"))
 	dfSubset = res[res$tag %in% tag,]
-	#dev.new()
-	#logi.hist.plot(dfSubset$act, dfSubset$targetP, boxp=T, type="hist", col="gray")
-	#title(main=tag)
-	mylogit = glm(targetP ~ sjiBody + sjiTitle + prior + offset, data=dfSubset, family="binomial")
+	mylogit = glm(model, data=dfSubset, family="binomial")
 	myPrint(summary(mylogit))
 	tmp = ClassLog(mylogit, dfSubset$targetP)
 	myPrint(tmp) 
@@ -43,12 +40,13 @@ logReg = function(tag, res) {
 	Ns = c(length(dfSubset[dfSubset$targetP == 1,]$targetP), length(dfSubset[dfSubset$targetP == 0,]$targetP), dim(dfSubset)[1])
 	names(Ns) = c('NTagged', 'NNotTagged', 'N')
 	#p = summary(mylogit)$coefficients["act",'Pr(>|z|)']
-	#return(with(tmp, data.frame(mcFadden=mcFadden,overall=overall, tag=tag, t(vals), t(Ns), p=p)))
 	return(list("logit"=mylogit, "classLog"=tmp))
 }
 
 addColumns = function(res) {
-	res$act=res$sjiTitle * coeffs$sjiTitle + res$sjiBody * coeffs$sjiBody + res$prior * coeffs$prior
+	coeffs = coeffs
+	coeffs[["offset"]] = NULL
+	res$act=computeAct(res, coeffs)
 	resTbl = data.table(res, key=c("tagFile"))
 	resTbl = resTbl[, mean := mean(act), by=c("tagFile")]
 	resTbl = resTbl[, median := median(act), by=c("tagFile")]
@@ -69,20 +67,69 @@ addColumnsPost = function(res){
 	return(res)
 }
 
-runLogReg = function(logRegFile, figName) {
-	res = read.csv(str_c(PATH, "/", logRegFile))
+computeAct = function(res, coeffs) {
+	print(coeffs)
+	act = 0
+	for(name in names(coeffs)) {
+		if( name == '(Intercept)') {
+			act = act+coeffs[[name]]
+		} else {
+			act = act+res[[name]]*coeffs[[name]]
+		}
+	}
+	act
+}
+
+runLogRegFrm = function(res, model=formula(targetP ~ sjiBody + sjiTitle + prior + offset)) {
 	res = addColumns(res)
-	logRegRes = logReg(res$tag, res)
-	coeffs = summary(logRegRes$logit)$coefficients[,"Estimate"]
-	res$act = res$sjiTitle * coeffs["sjiTitle"] + res$sjiBody * coeffs["sjiBody"] + res$prior * coeffs["prior"] + res$offset * coeffs["offset"]
-	res = addColumnsPost(res)
+	logRegRes = logReg(res$tag, res, model)
+	coeffs = as.list(summary(logRegRes$logit)$coefficients[,"Estimate"])
+	res$act = computeAct(res, coeffs)
+	return(append(logRegRes, list("res"=res, coeffs=coeffs)))	
+}
+
+runLogReg = function(logRegFile, figName) {
+	frm = read.csv(str_c(PATH, "/", logRegFile))
+	res = runLogRegFrm(frm)
 	asFig(figName)
 	logi.hist.plot(res$act, res$targetP, boxp=T, type="hist", col="gray")
 	devOff()
-	return(append(logRegRes, list("res"=res, coeffs=as.list(coeffs))))
+	return(res)
 }
 
-plotROCColumn = function(dFrame, column, cnt, col) {
+getFrms = function(frms=c(8:17)) {
+	lapply(frms, function(id) {read.csv(str_c(PATH, "/", "LogReg-", id, "-4-1.csv"))})
+}
+
+generateItems = function(model) {
+	frms = getFrms(c(8:9))
+	for(i in c(1:4)) {
+		res = runLogRegFrm(frms[[1]], model)
+		coeffs <<- res$coeffs
+	}
+	frms = adjustFrms(frms)
+	items = makeItems(frms)
+}
+
+adjustFrms = function(frms) {
+	foo = function(frm) {
+		frm = addColumns(frm)
+		frm$act = computeAct(frm, coeffs)
+		return(frm)		
+	}
+	lapply(frms, foo)
+}
+
+makeItems = function(frms) {
+	cnt = 0
+	foo = function(frm) {
+		cnt <<- cnt+1
+		list(act=frm$act, targetP=frm$targetP, tagFile=frm$tagFile, col=colors[cnt])
+	}
+	lapply(frms, foo)
+}
+
+plotROCColumn = function(dFrame, column) {
 	pred = prediction(dFrame[[column]], dFrame$targetP)
 	perf = performance(pred, "tpr", "fpr")
 	nSamples = length(unlist(perf@x.values))
@@ -92,6 +139,7 @@ plotROCColumn = function(dFrame, column, cnt, col) {
 	x = tp[indeces]+fp[indeces]
 	x = x/length(unique(dFrame$tagFile))
 	y = tp[indeces]/(tp[indeces]+fp[indeces])
+	return(list(x=x, y=y))
 	xlim = c(0, sum(dFrame$targetP)/length(unique(dFrame$tagFile)))
 	if(cnt != 1) {
 		lines(x, y, col=col, typ="p", pch=cnt)
@@ -100,13 +148,27 @@ plotROCColumn = function(dFrame, column, cnt, col) {
 	}		
 }
 
-plotROC = function(dFrame, columns) {
+colors = c("red", "green", "blue", "orange", "yellow", "black", "grey", "purple", "magenta", "cyan", "pink")
+
+plotROC2 = function(items) {
 	dev.new()
-	colors = c("red", "green", "blue")
-	for(column in columns){
-		ind = match(column, columns)
-		plotROCColumn(dFrame, column, ind, colors[ind]) 
+	colors = unlist(lapply(items, function(item) {item$col}))
+	foo = function(item) {
+		data.frame(act=item$act, targetP=item$targetP, tagFile=item$tagFile)
 	}
+	ROCs = lapply(items, function(item) {plotROCColumn(foo(item), "act")})
+	cnt = 0
+	columns=as.character(c(1:length(items)))
+	xlim = c(0, sum(items[[1]]$targetP)/length(unique(items[[1]]$tagFile)))
+	for(ROC in ROCs) {
+		cnt = cnt + 1
+		col = colors[cnt]
+		if(cnt != 1) {
+			lines(ROC$x, ROC$y, col=col, typ="p", pch=c(cnt, rep(NA, 9)))
+		} else {
+			plot(ROC$x, ROC$y, xlim=xlim, ylim=c(.3, 1), col=col, pch=c(cnt, rep(NA, 9)), xlab="average number of tags per post", ylab="model tag proportion correct")
+		}
+	}	
 	legend("topright", legend=columns, col=colors, pch=1:length(columns))
 }
 
