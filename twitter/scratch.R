49a9ed1aa866f648b0bfa31332b3a78e3945da4a
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-06-09 15:56:33 -0500

    scratch changes
    Former-commit-id: 338106c3dacf86a863154cf99de06baa208fa1d8 [formerly 589dc6e00cf32adf892b8e361f1d5df8fee14e38]
    Former-commit-id: c8b68d2776ffd1b6b42a20fb2c2a95187910ee07

--- twitter/scratch.R
+++ twitter/scratch.R
@@ -1,5 +1,12 @@
 consumerKey = "vKbz24SqytZnYO33FNkR7w"
 consumerSecret = "jjobro8Chy9aKMzo8szYMz9tHftONLRkjNnrxk0"
-foo = getTwitterOAuth(consumerKey, consumerSecret)
-registerTwitterOAuth(foo)
+twitCred = getTwitterOAuth(consumerKey, consumerSecret)
+
+save(list="twitCred", file="~/twitteR_credentials")
+
+load("~/twitteR_credentials")
+
+registerTwitterOAuth(twitCred)
+
+
 
