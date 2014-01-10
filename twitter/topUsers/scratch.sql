97009b28ccd4b97700fd909ff08d3aaaebcdfba4
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2014-01-09 18:38:09 -0600

    foo
    Former-commit-id: 93dfd56b51f2be77dfb2bd962322302973b17fd4 [formerly 9b7f432d9bafd9bc7ceb53f0f846b6d80db330a9]
    Former-commit-id: 28b192dd1fdc63d10a68edb7e681875429219cbc

---
+++ twitter/topUsers/scratch.sql
@@ -0,0 +1,25 @@
+
+select * from topUsers where username = ''; 
+select * from topUsers;
+
+select rank, t.user_screen_name, count(*) from tweets as t join topUsers as u on t.user_screen_name = u.user_screen_name group by t.user_screen_name, rank order by rank desc;
+select * from tweets where user_screen_name = 'JLo' limit 10;
+select count(*) from topUsers; 
+select count(*) from twitter_users;
+
+delete from topUsers;
+delete from twitter_users;
+select * from tweets;
+select user_screen_name from twitter_users;
+select count(*) from tweets;
+
+insert into tweets (id, created_at, text) values (5, 'created', 'text');
+insert into tweets (id, created_at, text) values (10, 'created', 'text');
+
+select * from tweets where user_screen_name = 'katyperry' order by id desc limit 1; 
+
+select * from tweets where id = '409332078233542656'
+
+select t.user_screen_name,rank,count(id) as count from tweets as t join topusers as u on t.user_screen_name = u.user_screen_name group by t.user_screen_name,rank order by rank desc;
+
+
