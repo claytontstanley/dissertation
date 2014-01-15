bfa4374cfe06529327c13b799428449af7258a85
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2014-01-15 17:58:11 -0600

    foo
    Former-commit-id: 5bda106377a2cf866990cbb7d29323e71c05dc74 [formerly 10d5a2562fe6cff5385a0d94a0db6a439ead1cd9]
    Former-commit-id: 54b82efc1d5b4d019fc0a4099043b35e8fa57aad

--- twitter/topUsers/preProcess.R
+++ twitter/topUsers/preProcess.R
@@ -273,7 +273,6 @@ getModelVsPredTbl <- function(modelHashtagsTbl) {
 genAggModelVsPredTbl <- function(hashtagsTbl, ds=c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,5,20)) {
 	genModelVsPredTbl <- function(hashtagsTbl, d, userScreenName) {
 		print(sprintf('generating model predictions for user %s', userScreenName))
-		print(hashtagsTbl)
 		modelHashtagsTbl = computeActsByUser(hashtagsTbl, d=d)
 		addMetrics(hashtagsTbl, modelHashtagsTbl)
 		modelVsPredTbl = getModelVsPredTbl(modelHashtagsTbl)	
