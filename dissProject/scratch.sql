178375ceeba6e26bfb00837adb3026cc3a893017
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2014-02-28 16:33:28 -0600

    foo
    Former-commit-id: 05076d01e287c9230d7c83b784853561b1e11fd1 [formerly 974a7f2cc61427459c4c5ae2306d2baf05eb3b75]
    Former-commit-id: a3fbd2945b3ed6f8f7dced2f0a9a0ff4d6ab7099

--- dissProject/scratch.sql
+++ dissProject/scratch.sql
@@ -1,3 +1,10 @@
+-- All ids in posts are present in post_tokenized, and vice versa, and none present in one and not the other (full outer join)
+select posts.id, posts.title from
+(select distinct(id) from post_tokenized) as t
+full outer join posts
+on t.id = posts.id
+where posts.post_type_id = 1
+and (t.id is null or posts.id is null) and posts.id not in (select post_filtered.post_id from post_filtered);
 
 select * from topUsers where username = ''; 
 select * from topUsers;
