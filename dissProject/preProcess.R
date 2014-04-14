59ba1c9cca5985854e2d35f5d343bee22d194de0
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2014-04-14 17:14:27 -0500

    foo

--- dissProject/preProcess.R
+++ dissProject/preProcess.R
@@ -1906,6 +1906,7 @@ computeActPermOrderless <- function(context, pos, config) {
 }
 
 curWS <- function() {
+	#FIXME: Add prior and orderless and prior and order to RP context runs
 	#FIXME: use d=c(.5,.7) and not just .5 for context runs. Will change results, but should only append values
 	# to both modelVsPredTbl and modelHashtagsTbl
 	#FIXME: Rename group name for first dataset
