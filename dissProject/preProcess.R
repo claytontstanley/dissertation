d5dd18b1a877ad49f9c60ad9b552c00d3362b6ac
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2014-07-01 14:42:11 -0500

    foo

--- dissProject/preProcess.R
+++ dissProject/preProcess.R
@@ -3576,8 +3576,6 @@ runGenAndSaveCurWorkspaceg4s6 <- function() genAndSaveCurWorkspace(groupConfigG4
 curWS <- function() {
 	# FIXME: Rerun context (s3 change, logregTbl change,  and name change from Hyman to Enthyman)
 	# FIXME: Rerun PUser (logregTbl change)
-	runPUserSOSji5k(regen='useAlreadyLoaded')
-	SOPUserSjigt5k.csv
 	withProf(runContext20g1s6(regen='useAlreadyLoaded'))
 	runContext500g1s6(regen='useAlreadyLoaded', numRunsT=1, numRunsSO=1)
 	runContext500g1s6(regen=F, numRunsT=1, numRunsSO=1)
